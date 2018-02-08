43d56df501580d663c70ba6b3aa9ff09e4a04c03
Alesconti <alesconti@gmail.com>
2018-02-08 14:02:20 +0100

    Order cancel

---
+++ odoo_order_cancel.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+print("Content-type:text/html\r\n")
+# import request
+# import sys
+import cgi, cgitb
+
+#recuper id ordine
+form = cgi.FieldStorage()
+ordine = form.getvalue('ordine')
+
+print(ordine)
+
+if ordine!=None:
+
+    url= 'http://34.240.79.218:8069'
+    db= 'odoo'
+    username='procedure@tyreo.com'
+    password='Fela1681'
+
+    import xmlrpclib
+    common = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(url))
+    common.version()
+    uid = common.authenticate(db, username, password, {})
+
+    models = xmlrpclib.ServerProxy('{}/xmlrpc/2/object'.format(url))
+    order_id = models.execute_kw(db, uid, password,'sale.order', 'search',[[['name', '=', ordine]]])
+    print(order_id)
+    print(models.execute_kw(db, uid, password, 'sale.order', 'action_cancel', order_id))
+else:
+    print("Parametro ordine non valorizzato")
